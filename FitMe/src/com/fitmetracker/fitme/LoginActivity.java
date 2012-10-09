54d1f21d1e1962192a963c8f9aa224ce45e315f3
Álvaro García <alvaroastur4@gmail.com>
2012-10-08 22:04:26 -0400

    Cambio A

--- FitMe/src/com/fitmetracker/fitme/LoginActivity.java
+++ FitMe/src/com/fitmetracker/fitme/LoginActivity.java
@@ -10,7 +10,7 @@ public class LoginActivity extends SherlockActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loginactivity);
+        setContentView(R.layout.loginactivity); // Cambio çlvaro
     }
 
    /* @Override
